e849cb6cbb8b381f5d4474f95eea02b102716279
kailashhari <h.kailash1501@gmail.com>
2023-02-11 04:06:59 +0530

    protected routes and auth setup

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom"
+import { LoginPage } from "./pages/Login";
+import { SignupPage } from "./pages/Signup";
+import { HomePage } from "./pages/Home";
+import { ProfilePage } from "./pages/Profile";
+import { SettingsPage } from "./pages/Settings";
+import { ProtectedLayout } from "./components/ProtectedLayout";
+import { HomeLayout } from "./components/HomeLayout";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <Routes>
+      <Route element={<HomeLayout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Route>
 
-export default App
+      <Route path="/dashboard" element={<ProtectedLayout />}>
+        <Route path="profile" element={<ProfilePage />} />
+        <Route path="settings" element={<SettingsPage />} />
+      </Route>
+    </Routes>
+  );
+}
---
+++ dashboard/src/hooks/useAuth.jsx
@@ -0,0 +1,63 @@
+import { createContext, useContext, useMemo } from "react";
+import { useNavigate } from "react-router-dom";
+import { useLocalStorage } from "./useLocalStorage";
+import axios from "axios";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useLocalStorage("user", null);
+  const navigate = useNavigate();
+
+  const login = async (data) => {
+    axios.post("http://localhost:8002/v1/user/signin", JSON.stringify({
+      email: data.email,
+      password: data.password
+    }), {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((response) => {
+      console.log(response);
+      setUser(data);
+      navigate("/dashboard/profile", { replace: true });
+    });
+  };
+
+  const signup = async (data) => {
+    axios.post("http://localhost:8002/v1/user/signup", JSON.stringify({
+      email: data.email,
+      name: data.name,
+      password: data.password
+    }), {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((response) => {
+      console.log(response);
+      setUser(data);
+      navigate("/dashboard/profile", { replace: true });
+    });
+  };
+
+  const logout = () => {
+    setUser(null);
+    navigate("/", { replace: true });
+  };
+
+  const value = useMemo(
+    () => ({
+      user,
+      login,
+      logout,
+      signup
+    }),
+    [user]
+  );
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
